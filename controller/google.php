7c3be95f14a0958494832a870d01886923d5d971
ziper <ipung1120@gmail.com>
2025-01-12 09:28:32 +0700

    ea

--- controller/google.php
+++ controller/google.php
@@ -2,17 +2,17 @@
 require_once '../vendor/autoload.php';
 session_start();
 
-$client = new Google_Client();
-$client->setClientId('xxxx');
-$client->setClientSecret('xxxx');
-$client->setRedirectUri('http://localhost/WhatsapiAPI/controller/oauth2callback.php');
-$client->addScope('email');
-$client->addScope('profile');
+//$client = new Google_Client();
+//$client->setClientId('854576687113-rs51j39mn5ek8cefdnu0es2bb5tm15lo.apps.googleusercontent.com');
+//$client->setClientSecret('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');
+//$client->setRedirectUri('http://localhost/WhatsapiAPI/controller/oauth2callback.php');
+//$client->addScope('email');
+//$client->addScope('profile');
 
 // Redirect ke Google untuk autentikasi
-if (!isset($_GET['code'])) {
-    $authUrl = $client->createAuthUrl();
-    header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
-    exit();
-}
+//if (!isset($_GET['code'])) {
+//    $authUrl = $client->createAuthUrl();
+//    header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+//    exit();
+//}
 ?>
