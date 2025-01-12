7da9d6300eb79d6e23ae97feeca49424fe660583
ziper <ipung1120@gmail.com>
2025-01-12 09:25:12 +0700

    update cuy

--- controller/google.php
+++ controller/google.php
@@ -3,8 +3,8 @@ require_once '../vendor/autoload.php';
 session_start();
 
 $client = new Google_Client();
-$client->setClientId('854576687113-rs51j39mn5ek8cefdnu0es2bb5tm15lo.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientId('');
+$client->setClientSecret('');
 $client->setRedirectUri('http://localhost/WhatsapiAPI/controller/oauth2callback.php');
 $client->addScope('email');
 $client->addScope('profile');
